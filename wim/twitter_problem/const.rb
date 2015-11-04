198141151bc0eebd62fd86b286d82c27021d16f5
sjyn <fsnewline@gmail.com>
2015-11-04 01:20:42 -0500

    fixing things one thing at a time

--- wim/twitter_problem/const.rb
+++
@@ -1,6 +0,0 @@
-class Constants
-	@@oh_one = "JtwwG40EW2O6CKBNp1qeD57IH"
-	@@oh_two = "NUSZ0jPyeWwTKPAlUDYLnoeatyDCsPp5waIJsYnQM6LJ5oDjDJ"
-	@@oh_three = "fKTi6UiCESwluB9UbBOsdcBVjHHPfUOxr6PbSWy4UCUi8"
-	@@oh_four = "3494713935-hoqlE0ocEarpGUbSosIEi3YJvHqrJH3j60x7Z74"
-end
--- wim/twitter_problem/twitter_stream.rb
+++ wim/twitter_problem/twitter_stream.rb
@@ -1,9 +1,10 @@
 require 'twitter'
+require_relative 'const.rb'
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "JtwwG40EW2O6CKBNp1qeD57IH"
-  config.consumer_secret     = "NUSZ0jPyeWwTKPAlUDYLnoeatyDCsPp5waIJsYnQM6LJ5oDjDJ"
-  config.access_token        = "3494713935-hoqlE0ocEarpGUbSosIEi3YJvHqrJH3j60x7Z74"
-  config.access_token_secret = "fKTi6UiCESwluB9UbBOsdcBVjHHPfUOxr6PbSWy4UCUi8"
+  config.consumer_key        = Constants.oh_one
+  config.consumer_secret     = Constants.oh_two
+  config.access_token        = Constants.oh_three
+  config.access_token_secret = Constants.oh_four
 end
 
 while true do
@@ -12,10 +13,10 @@ while true do
 			f.puts tweet.text
 		end
 	end
-	str = "Gathered Sample at " + Time.now.inspect
+	str = "Gathered Sample at #{Time.now.inspect}"
 	open('tw_log.txt','a') do |file|
 		file.puts str
 		puts str
 	end
-	sleep 1200
+	sleep 120
 end
