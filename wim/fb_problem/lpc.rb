8e1728635c0d5ec7c123df7d05ba1b6ddb87756f
sjyn <fsnewline@gmail.com>
2015-11-12 23:33:02 -0500

    so many thing

--- wim/fb_problem/lpc.rb
+++ wim/fb_problem/lpc.rb
@@ -3,22 +3,28 @@ require_relative 'daniel.rb'
 def count(m, s)
 	bitmap = Array.new(m)
 	i = 0
-	while i < m do
+	# while i < m do
+	# 	bitmap[i] = 0
+	# 	i += 1
+	# end
+	for i in 0..m do
 		bitmap[i] = 0
-		i += 1
 	end
 	s.each do |value|
 		address = m_hash(value, m)
 		bitmap[address] = 1
 	end
-	zeros = 0
+	zeros = 0.0
 	bitmap.each do |one_or_zero|
 		if one_or_zero == 0 then
-			zeros = zeros + 1
+			zeros += 1.0
 		end
 	end
-	lambda = zeros / m
-	cardinality = (-1 * m) * Math.log(lambda)
+	# puts "zeros: #{zeros}"
+	# puts "m: #{m}"
+	lamb = (zeros / m)
+	# puts "lambda: #{lamb}"
+	cardinality = (-1 * m) * Math.log(lamb)
 end
 
 def m_hash(value, size)
@@ -28,12 +34,13 @@ end
 def main
 	f = lambda do |x|
 		# Math::E ** x
-		x + 1
+		Math::E ** x
 	end
-	distribution = Distribution.new(3000, f)
-	set = distribution.count 100000
-	puts set.inspect
-	# puts count(1000000, set)
+	amnt = 7000000
+	distribution = Distribution.new(30000, f)
+	set = distribution.count amnt
+	#puts set.inspect
+	puts count(amnt, set)
 end
 
 main
