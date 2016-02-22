ce1bceda81db9a6c96f41e924a65d9f0ee35f979
Steven Rosendahl <sjyn@Lily.local>
2016-02-22 14:42:05 -0500

    final push for presentation

--- algorithms/projects/proj1/PJOne.java
+++ algorithms/projects/proj1/PJOne.java
@@ -16,26 +16,34 @@ import static java.lang.System.out;
 public class PJOne {
 
     public static void algoOne(int[] a){
+        int INST_COUT = 0;
+        //Begin counting instructions
         int n = a.length;
         int j = 1;
         int[] b = new int[n];
         while(j <= n - 1){
+            INST_COUT++;
             int i = 1;
             while(i <= n - j){
+                INST_COUT++;
                 int m = i + j - 1;
                 int r = min(m + j, n);
                 int u = i;
                 for(; u < r; u++){
+                    INST_COUT++;
                     b[u - 1] = a[u - 1];
                 }
                 u = i;
                 int v = m + 1;
                 int w = i;
                 for(; w < r; w++){
+                    INST_COUT++;
                     if((u > m) || (v <= r && b[v - 1] < b[u - 1])){
+                        INST_COUT++;
                         a[w - 1] = b[v - 1];
                         v++;
                     } else {
+                        INST_COUT++;
                         a[w - 1] = b[u - 1];
                         u++;
                     }
@@ -44,6 +52,7 @@ public class PJOne {
             }
             j *= 2;
         }
+        out.println(INST_COUT);
     }
 
     public static void compute(int[] a, int[] b, int i){
@@ -67,13 +76,13 @@ public class PJOne {
     }
 
     public static void main(String[] args){
-        int [] a = new int[]{12,-12,-55,0,-16,-189};
-        for(int aa : a)
-            out.printf("%6d", aa);
-        out.println();
+        int [] a = new int[]{-12,-12,-55,0,-16,-189, -42, -98};
+        // for(int aa : a)
+            // out.printf("%6d", aa);
+        // out.println();
         algoOne(a);
-        for(int aa : a)
-            out.printf("%6d",aa);
-        out.println();
+        // for(int aa : a)
+        //     out.printf("%6d",aa);
+        // out.println();
     }
 }
