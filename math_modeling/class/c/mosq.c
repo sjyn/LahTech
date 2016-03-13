8a3c8e480141c0b4831849f5ebea5e8797f6be07
Steven Rosendahl <sjyn@Lily.local>
2016-03-12 19:56:14 -0500

    finished paper three draft

---
+++ math_modeling/class/c/mosq.c
@@ -0,0 +1,26 @@
+/**
+This program attempts to solve the probability that a person lost in a forest will
+return to a river bank whle doing a ranndom walk
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define PI 3.14159265358979323846
+
+int main(){
+    //Generate random numbers with rand();
+    //In order to return to the river, the man needs to follow
+    // l * sin(theta1)
+    // The next day is
+    // l * sin(theta2)
+    // The sum
+    // lsin(theta1) + lsin(theta2) <= 0
+    // sin(theta1) + sin(theta2) <= 0
+
+    double theta1 = rand(); //* PI;
+    double theta2 = rand(); //* 2 * PI;
+    printf("theta1 = %f, theta2 = %f\n", theta1, theta2);
+
+    return 0;
+}
