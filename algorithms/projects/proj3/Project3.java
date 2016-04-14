1e537cde54ce427ed4062e59bef9a9676b7717d9
Steven Rosendahl <sjyn@Lily.local>
2016-04-14 14:53:20 -0400

    yolo

---
+++ algorithms/projects/proj3/Project3.java
@@ -0,0 +1,17 @@
+public class Project3 {
+
+    public static void main(String[] args){
+
+    }
+
+    public static void worstFit(){
+
+    }
+
+    public static void bestFit(){
+
+    }
+
+    
+
+}
